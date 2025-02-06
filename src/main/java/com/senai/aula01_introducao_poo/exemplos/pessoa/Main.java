cf67752946f6a3ae5af1d628147af05dd1602606
gabriel <gabrielnmyzy@gmail.com>
2025-02-06 11:01:21 -0300

    06/02

--- src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
@@ -1,7 +1,10 @@
 package com.senai.aula01_introducao_poo.exemplos.pessoa;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         Pessoa pessoa1 = new Pessoa(
                 "Gabriel",
@@ -29,12 +32,33 @@ public class Main {
                 )
         );
 
+
+        String[] etiquetas = {"nome: ", "idade: ", "altura: ", "endereço: \n\t rua: ", "\tnumero: ", "\tbairro: ", "\tcidade: ", "\testado: "};
+        String[] dados = new String[etiquetas.length];
+
+        System.out.println("Preencha o formulário: ");
+        for (int i = 0; i< etiquetas.length; i++){
+            System.out.println(etiquetas[i]);
+            dados[i] = sc.nextLine();
+        }
+        Pessoa pessoa3 = new Pessoa(
+                dados[0],
+                Integer.parseInt(dados[1]),
+                Float.parseFloat(dados[2]),
+                new Endereco(
+                        dados[3],
+                        Integer.parseInt(dados[4]),
+                        dados[5],
+                        dados[6],
+                        dados[7]
+                )
+
+        );
+        System.out.println(pessoa3);
+
         System.out.println(pessoa1.endereco.rua);
         pessoa1.falar("Olá!");
         pessoa2.falar("Oi, tudo bem?");
         pessoa1.comer("Maça");
-        pessoa2.comer("Hamburguer");
-
-        Pessoa pessoa3 = new Pessoa("Gabriel", 10);
     }
 }
