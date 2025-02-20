f9a39bccc747e51351d9a960b04ab159a650dc33
gabriel <gabrielnmyzy@gmail.com>
2025-02-20 11:02:21 -0300

    20/02

--- src/main/java/aula2_colecoes/exemplos/pessoa/exercicios/exercicio1/Tarefa.java
+++ src/main/java/aula2_colecoes/exemplos/pessoa/exercicios/exercicio1/Tarefa.java
@@ -3,7 +3,7 @@ package aula2_colecoes.exemplos.pessoa.exercicios.exercicio1;
 import java.util.ArrayList;
 
 public class Tarefa {
-    String tarefa;
+    String tarefaAdd;
     Boolean realizadas;
     ArrayList tarefas = new ArrayList();
 }
